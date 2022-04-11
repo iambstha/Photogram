0ed9e3ce30d94fdef4d5033cfeb7d01368103660
Bishal Shrestha <iambstha@gmail.com>
2022-04-11 21:53:10 +0545

    Only the boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
         <p>
           Welcome to my world
         </p>
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
       </header>
     </div>
   );
